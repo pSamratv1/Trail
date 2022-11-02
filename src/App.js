31db8e59075548b2d9cef0bb55da3916d3719e1d
CODE-SAMRAT <101516967+CODE-SAMRAT@users.noreply.github.com>
2022-11-02 20:22:14 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,88 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState("");
+  const [ItemList, setItemList] = useState([]);
+
+  const addItem = () => {
+    if (!text) {
+    } else setItemList([...ItemList, text]);
+    setText("");
+  };
+  const deleteItem = (id) => {
+    const afterDeletedList = ItemList.filter((element, keys) => {
+      return keys !== id;
+    });
+    setItemList(afterDeletedList);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='todo-head'>
+        <h1
+          className='todo-title'
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginLeft: 600,
+            marginTop: 100,
+            width: 200,
+          }}>
+          Todo Lists
+        </h1>
+        <div
+          className='todo-body'
+          style={{
+            width: 400,
+            height: 200,
+            marginLeft: 500,
+            marginTop: 100,
+          }}>
+          <div
+            className='todo-input-text'
+            style={{
+              display: "flexbox",
+              justifyContent: "center",
+            }}>
+            <input
+              type='text'
+              placeholder='Add your list here....'
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </div>
+          <button
+            className='btn'
+            style={{backgroundColor: "#008CBA"}}
+            onClick={addItem}>
+            Add
+          </button>
+          <div className='todo-items'>
+            {ItemList.map((element, keys) => {
+              return (
+                <div
+                  className='todo-individual-list'
+                  style={{
+                    display: "flexbox",
+                    marginTop: 10,
+                    border: "1px solid green",
+                  }}
+                  key={keys}>
+                  {element}
+                  <button
+                    className='btn btn-danger'
+                    style={{
+                      display: "flex",
+                      backgroundColor: "#f44336",
+                    }}
+                    onClick={() => deleteItem(keys)}>
+                    Delete
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
