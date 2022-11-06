c41ea381ca8ef4baf3e7e7dfbb115b2a4d9685fd
CODE-SAMRAT <101516967+CODE-SAMRAT@users.noreply.github.com>
2022-11-06 19:32:51 +0545

    Components added need to fix some errors

---
+++ components/Text.js
@@ -0,0 +1,75 @@
+import React from "react";import {useState} from "react";
+import Todo1 from "./Todo1";
+
+const Text = () => {
+  let count = 0;
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("");
+  const handleSubmit = (e) => {
+    setText(text);
+    setCategory(category);
+    count = count + 1;
+  };
+
+  return (
+    <div>
+      <div
+        className='todo-body'
+        style={{
+          width: 400,
+          height: 200,
+          marginLeft: 500,
+        }}>
+        <div
+          className='todo-input-text'
+          style={{
+            display: "flexbox",
+            justifyContent: "center",
+          }}>
+          <input
+            type='text'
+            style={{
+              height: 30,
+              marginLeft: 120,
+              display: "flex",
+            }}
+            placeholder='Add your Todo here....'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <input
+            type='text'
+            style={{
+              marginTop: 5,
+              height: 30,
+              marginLeft: 120,
+            }}
+            placeholder='Add your Category here....'
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+        </div>
+
+        <button
+          className='btn'
+          style={{
+            backgroundColor: "#008CBA",
+            blockSize: 35,
+            marginTop: 10,
+            marginLeft: 150,
+            width: 100,
+            paddingLeft: 10,
+            paddingRight: 10,
+            borderRadius: 10,
+          }}
+          onSubmit={handleSubmit()}>
+          Add
+        </button>
+      </div>
+      if count === 1 return (<Todo1 text={text} category={category} />
+      );
+    </div>
+  );
+};
+
+export default Text;
---
+++ components/Todo1.js
@@ -0,0 +1,98 @@
+import React from "react";
+import {useState} from "react";
+
+const Todo1 = ({text, category}) => {
+  let complete = false;
+  const [ItemList, setItemList] = useState([{text, category, complete}]);
+
+  return (
+    <>
+      <div className='todo-items'>
+        {ItemList.map((element, keys) => {
+          return (
+            <div
+              className='todo-individual-list'
+              style={{
+                display: "flexbox",
+                marginTop: 10,
+                border: "1px solid green",
+                borderRadius: 20,
+                backgroundColor: "#cdbc1d",
+                color: "#0e1aed",
+                fontSize: 15,
+              }}
+              key={keys}>
+              <span
+                style={{
+                  fontSize: 25,
+                  fontWeight: 600,
+                  marginLeft: 10,
+                }}>
+                {keys + 1}.{element.text}
+              </span>
+              <p
+                className='category-text'
+                style={{
+                  fontSize: 15,
+                  marginLeft: 50,
+                  fontWeight: 500,
+                }}>
+                Category:-<span> </span>
+                {element.category}
+              </p>
+              <input
+                name='checkbox'
+                type='checkbox'
+                checked={element.completion}
+                style={{
+                  width: 15,
+                  height: 15,
+                  marginLeft: 300,
+                }}
+                onChange={() => {
+                  const changeHandler = (id) => {
+                    const afterChangedList = ItemList.filter(
+                      (element, keys) => {
+                        if (keys === id) {
+                          return (element.complete = !element.complete);
+                        }
+                        return setItemList(afterChangedList);
+                      }
+                    );
+                  };
+                  changeHandler(keys);
+                }}
+              />
+              Completed
+              <button
+                className='btn btn-danger'
+                style={{
+                  backgroundColor: "#f44336",
+                  marginLeft: 150,
+                  blockSize: 35,
+                  borderRadius: 10,
+                  paddingLeft: 10,
+                  paddingRight: 10,
+                }}
+                onClick={() => {
+                  const deleteItem = (id) => {
+                    const afterDeletedList = ItemList.filter(
+                      (element, keys) => {
+                        return keys !== id;
+                      }
+                    );
+                    afterDeletedList();
+                  };
+                  deleteItem(keys);
+                }}>
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Todo1;
