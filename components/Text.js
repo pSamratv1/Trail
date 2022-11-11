819b1eca8833c5887a5f8e02505cc12c3c86a82e
CODE-SAMRAT <101516967+CODE-SAMRAT@users.noreply.github.com>
2022-11-11 17:28:56 +0545

    Update Text.js

--- components/Text.js
+++ components/Text.js
@@ -1,73 +1,81 @@
 import React from "react";import {useState} from "react";
-
+import Todo1 from "./Todo1";
 
 const Text = () => {
-  let count = 0;
   const [text, setText] = useState("");
   const [category, setCategory] = useState("");
-  const handleSubmit = (e) => {
-    setText(text);
-    setCategory(category);
-    count = count + 1;
-  };
-  return (
-    <div>
-      <div
-        className='todo-body'
-        style={{
-          width: 400,
-          height: 200,
-          marginLeft: 500,
-        }}>
-        <div
-          className='todo-input-text'
-          style={{
-            display: "flexbox",
-            justifyContent: "center",
-          }}>
-          <input
-            type='text'
-            style={{
-              height: 30,
-              marginLeft: 120,
-              display: "flex",
-            }}
-            placeholder='Add your Todo here....'
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
-          <input
-            type='text'
-            style={{
-              marginTop: 5,
-              height: 30,
-              marginLeft: 120,
-            }}
-            placeholder='Add your Category here....'
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-          />
-        </div>
+  const [ItemList, setItemList] = useState([]);
 
-        <button
-          className='btn'
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setItemList([...ItemList, {category, text}]);
+    setText("");
+    setCategory("");
+  };
+
+  return (
+    <>
+      <div>
+        <div
+          className='todo-body'
           style={{
-            backgroundColor: "#008CBA",
-            blockSize: 35,
-            marginTop: 10,
-            marginLeft: 150,
-            width: 100,
-            paddingLeft: 10,
-            paddingRight: 10,
-            borderRadius: 10,
-          }}
-          onSubmit={handleSubmit()}>
-          Add
-        </button>
+            width: 400,
+            height: 200,
+            marginLeft: 500,
+          }}>
+          <div
+            className='todo-input-text'
+            style={{
+              display: "flexbox",
+              justifyContent: "center",
+            }}>
+            <input
+              type='text'
+              style={{
+                height: 30,
+                marginLeft: 120,
+                display: "flex",
+              }}
+              placeholder='Add your Todo here....'
+              value={text}
+              onChange={(e) => {
+                setText(e.target.value);
+              }}
+            />
+            <input
+              type='text'
+              style={{
+                marginTop: 5,
+                height: 30,
+                marginLeft: 120,
+              }}
+              placeholder='Add your Category here....'
+              value={category}
+              onChange={(e) => {
+                setCategory(e.target.value);
+              }}
+            />
+          </div>
+
+          <button
+            className='btn'
+            style={{
+              backgroundColor: "#008CBA",
+              blockSize: 35,
+              marginTop: 10,
+              marginLeft: 150,
+              width: 100,
+              paddingLeft: 10,
+              paddingRight: 10,
+              borderRadius: 10,
+            }}
+            onClick={handleSubmit}>
+            Add
+          </button>
+        </div>
       </div>
-      <Todo1 text= {text} category={category}/>
-      );
-    </div>
+      {/* <Todo1 list={ItemList} /> */}
+    </>
   );
 };
 
